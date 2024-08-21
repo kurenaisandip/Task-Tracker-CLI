bba6bbfed9a65894aaa57e2679bb60b0d1f80005
Sandip <srai68141@gmail.com>
2024-08-21 17:53:26 +0545

    added model in the project to perform crud operation in the json

---
+++ Model/ToDos.cs
@@ -0,0 +1,10 @@
+public class ToDos
+{
+
+    public int Id { get; set; }
+    public string? name { get; set; }
+    public string? description { get; set; }
+    public string? status { get; set; }
+    public DateTime createdAt { get; set; }
+    public DateTime updatedAt { get; set; }
+}
