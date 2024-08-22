041764a6f6fc511e70ac2598844aa48606882d60
Sandip <srai68141@gmail.com>
2024-08-22 10:28:59 +0545

    added default value for status

--- Model/ToDos.cs
+++ Model/ToDos.cs
@@ -4,7 +4,7 @@ public class ToDos
     public int Id { get; set; }
     public string? name { get; set; }
     public string? description { get; set; }
-    public string? status { get; set; }
+    public string? status { get; set; } = "todo"; // defaault status is todo
     public DateTime createdAt { get; set; }
     public DateTime updatedAt { get; set; }
 }
